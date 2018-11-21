1ff3ad6bbbb91290b827cbb309171d2b43f63a38
inherited-firmware-6870 <johnhaeng@gmail.com>
2018-11-21 00:22:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,57 @@
-function dwarfRollCall(dwarves) {
+function dwarfRollCall (dwarves) {
+  var rollCallArr = [];
+  
+  for (var i = 0; i < dwarves.length; i += 1) {
+    var dwarf = i + 1 + `. ` + dwarves[i] + ` `;
+    
+    rollCallArr.push(dwarf);
+  }
+  
+  return rollCallArr.join('');
 }
 
-function summonCaptainPlanet(planeteerCalls){
+
+
+function summonCaptainPlanet (planeteerCalls){
+  var cappedAndExp = [];
+  
+  for (var i = 0; i < planeteerCalls.length; i += 1) {
+    var planeteerCall = planeteerCalls[i].toUpperCase();
+    
+    cappedAndExp.push(planeteerCall + `!`);
+  }
+  
+  return cappedAndExp;
 }
 
-function longPlaneteerCalls(words) {
+
+
+function longPlaneteerCalls (words) {
+  var count = 0;
+  
+  for (var i = 0; i < words.length; i += 1) {
+    var word = words[i];
+    
+    if (word.length > 4) {
+      count += 1;
+    }
+  }
+  
+  return count > 0;
 }
+
+
 
 function findTheCheese (foods) {
+  var cheeses = [`cheddar`, `gouda`, `camembert`];
+  
+  for (var i = 0; i < foods.length; i += 1) {
+    var food = foods[i];
+    
+    if (cheeses.indexOf(food) > -1) {
+      return food;
+    }
+  }
+  
+  return 'no cheese!';
 }
